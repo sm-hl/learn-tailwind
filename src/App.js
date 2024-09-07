85094763e9ef3aace3cc4a50556e0810c63a2ec7
Salma Halhoul <halhoulsa00@gmail.com>
2024-09-07 01:05:35 +0000

    add smooth scroll & add font family downloaded & active link onscroll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    window.onscroll = () => {
+      sections.forEach(section => {
+        const top = window.scrollY;
+        const offset = section.offsetTop-60;//why minus 60 of height of header through has fixed  position
+        const height = section.offsetHeight;
+        const id = section.getAttribute('id');
+        if (top > offset && top < offset + height) {
+          navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.href.includes(id)) {
+              link.classList.add('active');
+            }
+          }
+          )
+        }
+      })
+    }
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav className='flex justify-between p-4 bg-black text-white fixed top-0 right-0 left-0'>
+      <h2>Logo</h2>
+      <ul className='flex space-x-12 items-center'>
+        <li><a href="#home" className='nav-link hover:text-blue-400'>Home</a></li>
+        <li><a href="#about" className='nav-link hover:text-blue-400'>About</a></li>
+        <li><a href="#reviews" className='nav-link hover:text-blue-400'>Reviews</a></li>
+      </ul>
+    </nav>
+    <section id='home' className='h-screen bg-black text-white flex justify-center items-center text-3xl'>Home</section>
+    <section id='about' className='h-screen bg-gray-900 text-white flex justify-center items-center text-3xl'>about</section>
+    <section id='reviews' className='h-screen bg-black text-white flex justify-center items-center text-3xl'>reviews</section>
+    </>
   );
 }
 
